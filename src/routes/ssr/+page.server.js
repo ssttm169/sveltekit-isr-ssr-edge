18f04886d506f01c17fcfc64117a17ea1c4c4acd
tomtan <royongbin@qq.com>
2023-04-14 22:00:49 +0800

    add form

--- src/routes/ssr/+page.server.js
+++ src/routes/ssr/+page.server.js
@@ -1,4 +1,8 @@
-export function load() {
+// @ts-nocheck
+import { fail } from '@sveltejs/kit';
+
+
+export async function load() {
 	const generatedAt = new Date().toLocaleString('en-GB', {
 		day: '2-digit',
 		month: '2-digit',
@@ -9,7 +13,43 @@ export function load() {
 		fractionalSecondDigits: 3 // for milliseconds
 	});
 
+	const res = await Promise.resolve('yes')
+
 	return {
-		generatedAt
+		generatedAt,
+		res
 	};
+
+
 }
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+    login: async ({  request }) => {
+
+		console.log('login')
+        const data = await request.formData();
+        const ipAddress = data.get('ipAddress');
+        const port = data.get('port');
+
+        if (!ipAddress) {
+            return fail(400, { ipAddress, missing: true });
+        }
+
+        if (!port) {
+            return fail(400, { port, missing: true });
+        }
+
+
+        const execRes = await Promise.resolve('yes');
+
+    
+        console.log('execRes', execRes)
+
+        return { success: true, execRes };
+    },
+    register: async () => {
+        // TODO register the user
+        console.log('register=====')
+    }
+};
--- src/routes/ssr/+page.svelte
+++ src/routes/ssr/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	/** @type {import('./$types').PageData} */
 	export let data;
+	export let form;
 </script>
 
 <div>
@@ -18,6 +19,54 @@
 		<div class="mb-2 text-sm uppercase text-white/60">Current Date:</div>
 		<div class="text-2xl">
 			{data.generatedAt}
+			{data.res}
 		</div>
 	</div>
+
+	<div class="page-container-aside container mx-auto p-8 space-y-8">
+		<div class="previewer shadow-2xl shadow-surface-500/10 dark:shadow-black/10 rounded-container-token overflow-hidden">
+			<div class="card p-4 bg-initial">
+				<section class="p-4">
+	
+					{#if form?.incorrect}<p class="error">
+							Invalid credentials!
+						</p>{/if}
+					<form method="POST" action="?/login">
+						<div class="space-y-4">
+							<label class="label">
+								<span>IP地址</span>
+								<input
+									class="input"
+									type="text"
+									placeholder="请输入IP地址"
+									name="ipAddress"  value={form?.ipAddress ?? ""} 
+								/>
+								{#if form?.missing}<p class="error">The ipAddress field is required</p>{/if}
+							</label>
+	
+							<label class="label">
+								<span>端口号</span>
+								<input
+									class="input"
+									type="text"
+									placeholder="请输入端口号"
+									name="port" 
+								/>
+							</label>
+							<button   class="btn variant-filled">提交</button>
+						</div>
+					</form>
+					{#if form?.success}
+						<!-- this message is ephemeral; it exists because the page was rendered in
+						   response to a form submission. it will vanish if the user reloads -->
+						{form?.execRes}
+						<p>Successfully logged in! Welcome back, tom</p>
+					{/if}
+				</section>
+			</div>
+		</div>
+	</div>
+
+
+
 </div>
