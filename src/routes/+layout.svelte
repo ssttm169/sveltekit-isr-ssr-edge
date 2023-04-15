01b876cb6c12318d19876c8a6c5860751aca3728
tomtan <royongbin@qq.com>
2023-04-15 23:16:47 +0800

    test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,15 +3,13 @@
 	import "../part1.css";
 	import "../global.css";
 	import "../settings.css";
-	import WindowContent from "../lib/modules/WindowContent.svelte";
 </script>
 
 <div class="home_container__Myqoy">
 	<div class="home_sidebar__4L4yT home_sidebar-show__I77cg">
-		<!-- <button on:click={roll}>Roll the dice</button> -->
-		<slot />
+		11
 	</div>
 	<div class="home_window-content__RRVow">
-		<WindowContent />
+		22
 	</div>
 </div>
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,31 +1,4 @@
-// import * as database from '$lib/server/database.js';
-
 export async function load() {
-    // const generatedAt = new Date().toLocaleString('en-GB', {
-    // 	day: '2-digit',
-    // 	month: '2-digit',
-    // 	year: 'numeric',
-    // 	hour: '2-digit',
-    // 	minute: '2-digit',
-    // 	second: '2-digit',
-    // 	fractionalSecondDigits: 3 // for milliseconds
-    // });
-
-    // const res = await Promise.resolve('yes')
-
-    // return {
-    // 	generatedAt,
-    // 	res
-    // };
-
-
-    // let userid = cookies.get('userid');
-
-    // if (!userid) {
-    // 	userid = crypto.randomUUID();
-    // 	cookies.set('userid', userid, { path: '/' });
-    // }
-
     const linkData = [
         {
             id: 1,
